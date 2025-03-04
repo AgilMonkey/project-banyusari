2c4eaba5fb3b7d407b517a2839a76e715921a1ba
Aepu <muhammadagilfadilah@gmail.com>
2025-03-04 23:06:11 +0800

    Add simple movement

--- player/third_person_movement_rigidbody.gd
+++ player/third_person_movement_rigidbody.gd
@@ -1,8 +1,20 @@
 extends Node
 
 
+@export var speed := 50
+var input_dir: Vector3
+
 var rb: RigidBody3D
 
 
 func _ready() -> void:
 	rb = get_parent()
+
+
+func _input(event: InputEvent) -> void:
+	input_dir.z = Input.get_axis("move_forward", "move_backward")
+	input_dir.x = Input.get_axis("move_left", "move_right")
+
+
+func _process(delta: float) -> void:
+	rb.apply_force(input_dir * speed)
