0d2180e96febda7848bd1de4195d548c6b0600e5
Aepu <muhammadagilfadilah@gmail.com>
2025-03-04 22:47:21 +0800

    Change name

--- player/third_person_movement.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-var character: CharacterBody3D
-
-
-func _ready() -> void:
-	character = get_parent()
---
+++ player/third_person_movement_rigidbody.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+var rb: RigidBody3D
+
+
+func _ready() -> void:
+	rb = get_parent()
