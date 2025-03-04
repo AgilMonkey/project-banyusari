f7aa2fc7eed56c790c1fc1470998fc8decb18fdb
Aepu <muhammadagilfadilah@gmail.com>
2025-03-04 23:22:02 +0800

    add weird rb stuff? Idk just testing things out

--- player/third_person_movement_rigidbody.gd
+++ player/third_person_movement_rigidbody.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-@export var speed := 20
+@export var speed := 10
 var input_dir: Vector3
 
 var rb: RigidBody3D
@@ -14,7 +14,15 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	input_dir.z = Input.get_axis("move_forward", "move_backward")
 	input_dir.x = Input.get_axis("move_left", "move_right")
+	input_dir = input_dir.normalized()
 
 
 func _process(delta: float) -> void:
+	rb.linear_damp = 0.0
+	if input_dir.length_squared() == 0:
+		rb.linear_damp = 10.0
+	
+	if rb.linear_velocity.length() > 20.0:
+		rb.linear_damp = 10.0
+
 	rb.apply_force(input_dir * speed)
