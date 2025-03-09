d12359a6f92f2b3980b06fd2d23dc03ea9d80bfa
Aepu <muhammadagilfadilah@gmail.com>
2025-03-09 10:28:45 +0800

    Add simple health component

---
+++ commons/components/health/health_component.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+signal health_reached_zero
+
+@export var max_health: int = 100
+var cur_health: int
+
+
+func _ready() -> void:
+	cur_health = max_health
+
+
+func add(val):
+	cur_health += val
+	cur_health = min(cur_health, max_health)
+
+
+func damage(val):
+	cur_health -= val
+	if cur_health <= 0:
+		health_reached_zero.emit()
