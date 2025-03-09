3d3b53f59a6bdad83e41fe19980e61e7bf1242a0
Aepu <muhammadagilfadilah@gmail.com>
2025-03-10 07:01:27 +0800

    Membuat mulai agak simple dan ganti jadi v0.4

--- world/main.gd
+++ world/main.gd
@@ -5,10 +5,19 @@ extends Node3D
 
 
 func _ready() -> void:
+	for enemy in get_tree().get_nodes_in_group("enemy"):
+		enemy.process_mode = Node.PROCESS_MODE_DISABLED
+	
 	player.health.health_reached_zero.connect(game_over)
 	player.health.health_changed.connect(func(val):
 		$CanvasLayer/UI/HealthbarUI.set_healthbar_value(val)
 		)
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_L):
+		for enemy in get_tree().get_nodes_in_group("enemy"):
+			enemy.process_mode = Node.PROCESS_MODE_INHERIT
 
 
 func game_over():
