6471010697002e558fbd25d2c468cca82abd38d5
Aepu <muhammadagilfadilah@gmail.com>
2025-03-14 22:52:30 +0800

    Change rb to character body. Then refactor some scripts

---
+++ entities/player/player2.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+
+var on_floor = false
+
+@onready var health := $HealthComponent
+
+
+func disable_player():
+	enable_input(self, false)
+
+
+func enable_input(node, enable):
+	node.set_process_input(enable)
+	for n in node.get_children():
+		enable_input(n, enable)
+
+
+func _on_player_hitbox_on_hit(damage: Variant) -> void:
+	health.damage(damage)
