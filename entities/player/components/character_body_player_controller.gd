4486c8bf9e964bb899b08c2fe89f45ea6bed83ff
Aepu <muhammadagilfadilah@gmail.com>
2025-03-24 13:15:33 +0800

    Added simple input

--- entities/player/components/character_body_player_controller.gd
+++ entities/player/components/character_body_player_controller.gd
@@ -1,6 +1,20 @@
 extends Node
 
 
-@export var speed: = 10
+@export var speed := 10
+
+var input_dir := Vector2.ZERO
 
 @onready var player: CharacterBody3D = $".."
+
+
+func _input(event: InputEvent) -> void:
+	# Controller
+	input_dir = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+	
+	if Input.is_action_just_pressed("jump"):
+		print("jump")
+
+
+func _physics_process(delta: float) -> void:
+	print(input_dir)
