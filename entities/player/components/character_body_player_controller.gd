c5847c371146a0a9641b9726ad70d3892f4550f6
Aepu <muhammadagilfadilah@gmail.com>
2025-03-24 13:33:03 +0800

    Add gravity and jump

--- entities/player/components/character_body_player_controller.gd
+++ entities/player/components/character_body_player_controller.gd
@@ -1,7 +1,8 @@
 extends Node
 
 
-@export var speed := 10
+@export var speed := 14.0
+@export var jump_force := 15.0
 
 var input_dir := Vector3.ZERO
 
@@ -15,11 +16,25 @@ func _input(event: InputEvent) -> void:
 	input_dir = Vector3(inp_flat.x, 0, -inp_flat.y)
 	
 	if Input.is_action_just_pressed("jump"):
-		print("jump")
+		c_body.velocity.y = jump_force
 
 
 func _physics_process(delta: float) -> void:
+	horizontal_movement()
+	gravity(delta)
+	
+	c_body.move_and_slide()
+
+
+func horizontal_movement():
 	var cam_rotation_y = cur_camera.rotation.y
 	var cam_direction = input_dir.rotated(Vector3.UP, cam_rotation_y)
-	c_body.velocity = cam_direction * speed
-	c_body.move_and_slide()
+	var from_cam_vel = cam_direction * speed
+	
+	c_body.velocity.x = from_cam_vel.x
+	c_body.velocity.z = from_cam_vel.z
+
+
+func gravity(delta):
+	var down_force = ProjectSettings.get_setting("physics/3d/default_gravity")
+	c_body.velocity.y -= down_force * delta
