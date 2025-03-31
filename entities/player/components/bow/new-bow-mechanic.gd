201614b18e5842e144885399f39600a57a2393ff
Aepu <muhammadagilfadilah@gmail.com>
2025-03-31 09:38:15 +0800

    Add input timer and bow percent

--- entities/player/components/bow/new-bow-mechanic.gd
+++ entities/player/components/bow/new-bow-mechanic.gd
@@ -1,5 +1,29 @@
 extends Node3D
 
-
 # TODO
 # 1. Value for how long input is pressed
+
+@export var max_bow_t := 3.0
+
+var inp_hold_bow := false
+var bow_hold_t := 0.0
+var bow_hold_p := 0.0:
+	get:
+		return clampf(bow_hold_t / max_bow_t, 0.0, 1.0)
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		inp_hold_bow = true
+	elif Input.is_action_just_released("shoot"):
+		var bow_hold_percent = clampf(bow_hold_t / 1.0, 0.0, 1.0)
+		
+		bow_hold_t = 0.0
+		inp_hold_bow = false
+
+
+func _process(delta: float) -> void:
+	if inp_hold_bow:
+		bow_hold_t += delta
+	
+	print(bow_hold_p)
