3a8629ceb9c303c38c7eeddb0c6631be1aa26375
Aepu <muhammadagilfadilah@gmail.com>
2025-04-01 15:16:38 +0800

    Added the cool line sim stuff. Now just have to make it like the real one

--- entities/player/components/bow/new-bow-mechanic.gd
+++ entities/player/components/bow/new-bow-mechanic.gd
@@ -6,6 +6,10 @@ extends Node3D
 # - Get the forward point
 # - Add line
 
+# Based on 20 physics step
+var SIM_RANGE = 240
+var SIM_DELTA = 0.05
+
 @export var max_bow_t := 3.0
 @export var min_bow_power := 10.0
 @export var max_bow_power := 40.0
@@ -37,32 +41,30 @@ func _process(delta: float) -> void:
 	if inp_hold_bow:
 		bow_hold_t += delta
 	
-	var forward_bit_up = -basis.z
-	var sim_verts = get_simulation_verts(forward_bit_up * 20.0, 24.0)
+	bow_look_at_forward_cam()
+	
+	var sim_verts = get_simulation_verts(-basis.z * 100.0, 24.0)
 	
 	$Line3D.points = sim_verts
 	$Line3D.rebuild()
 
 
-func get_bow_direction() -> Vector3:
+func bow_look_at_forward_cam() -> void:
 	var camera_forward_range := cur_cam.global_position + (-cur_cam.transform.basis.z * bow_forward_range) 
-	return camera_forward_range - global_position
+	look_at(camera_forward_range)
 
 
 func get_simulation_verts(force: Vector3, gravity: float) -> PackedVector3Array:
 	var sim_verts: PackedVector3Array = PackedVector3Array()
 	
-	var sim_delta = 0.05
 	var velocity = force
 	var sim_position = Vector3.ZERO
 	
 	sim_verts.append(sim_position)
 	
-	var simulation_range = 200  # Divisible by 20
-	
-	for n in simulation_range - 1:
-		sim_position += velocity * sim_delta
-		velocity.y -= gravity * sim_delta
+	for n in SIM_RANGE - 1:
+		sim_position += velocity * SIM_DELTA
+		velocity.y -= gravity * SIM_DELTA
 		
 		sim_verts.append(sim_position)
 	
